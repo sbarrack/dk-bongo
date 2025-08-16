dbb2a84c2ef07ae764661c9c110db5dab8c9d36c
sbarrack <12619603+sbarrack@users.noreply.github.com>
2025-08-16 12:42:39 -0700

    tidy blink

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,12 +1,10 @@
-const int led = LED_BUILTIN;
-
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
-  digitalWrite(led, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
 }
