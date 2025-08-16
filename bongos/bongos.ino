2fa992da8b9dd582be32c10ef4c5392e98717669
sbarrack <12619603+sbarrack@users.noreply.github.com>
2025-08-15 18:10:34 -0700

    usb joystick + elapsed micros example + cleanup

--- bongos/bongos.ino
+++ bongos/bongos.ino
@@ -11,11 +11,7 @@
 
 // https://wiibrew.org/wiki/Wiimote/Extension_Controllers
 #define I2C_SLAVE_ADDRESS_WII_ATTACHMENT 0x52
-/**
- * This worked as low as 34us on USB power, but it might need more grace on
- * lower CPU speeds which varies with Vin.
- */
-#define I2C_TX_GRACE_TIME_MICROS 200
+#define I2C_TX_GRACE_TIME_MICROS 50
 #define WII_ATTACHMENT_ID_SIZE 6
 
 const uint8_t CONTROLLER_ID_NUNCHUCK[WII_ATTACHMENT_ID_SIZE] = {0x00, 0x00, 0xA4, 0x20, 0x00, 0x00};
@@ -39,10 +35,7 @@ union NunchuckPacket {
     };
 };
 
-/**
- * @param _wireNumber For debugging
- */
-bool decrypt(TwoWire &i2c, int _wireNumber = 0) {
+bool decrypt(TwoWire &i2c) {
     int errorCode;
 
     i2c.beginTransmission(I2C_SLAVE_ADDRESS_WII_ATTACHMENT);
@@ -51,10 +44,6 @@ bool decrypt(TwoWire &i2c, int _wireNumber = 0) {
     errorCode = i2c.endTransmission();
     delayMicroseconds(I2C_TX_GRACE_TIME_MICROS);
     if (errorCode != E_I2C_TX_SUCCESS) {
-        // Serial.print("Decrypting Wire");
-        // Serial.print(_wireNumber, DEC);
-        // Serial.print(" part 1/2 failed with code: ");
-        // Serial.println(errorCode);
         return false;
     }
 
@@ -63,12 +52,6 @@ bool decrypt(TwoWire &i2c, int _wireNumber = 0) {
     i2c.write(0x00);
     errorCode = i2c.endTransmission();
     delayMicroseconds(I2C_TX_GRACE_TIME_MICROS);
-    // if (errorCode != E_I2C_TX_SUCCESS) {
-    //     Serial.print("Decrypting Wire");
-    //     Serial.print(_wireNumber, DEC);
-    //     Serial.print(" part 2/2 failed with code: ");
-    //     Serial.println(errorCode);
-    // }
 
     return errorCode == E_I2C_TX_SUCCESS;
 }
@@ -76,23 +59,20 @@ bool decrypt(TwoWire &i2c, int _wireNumber = 0) {
 void setup() {
     // Fast mode
     Wire.setClock(400000);
-    // Wire1.setClock(400000);
+    Wire1.setClock(400000);
 
     Wire.begin();
-    // Wire1.begin();
+    Wire1.begin();
     
     Serial.begin(115200);
-    while (!Serial);
-    delay(2000);
     // make && sleep 1 && screen /dev/ttyACM0 115200
 }
 
-// FIXME handle rx fail
-// FIXME handle tx error based on code
-// FIXME multiple controllers
 bool isEncrypted = true;
 bool isNunchuck = false;
+elapsedMicros loopTime; // TODO use this to make a timed queue for rx/tx
 void loop() {
+    loopTime = 0;
     isEncrypted = !decrypt(Wire);
     if (isEncrypted) return;
 
@@ -101,7 +81,7 @@ void loop() {
         Wire.write(0xFA);
         Wire.write(0x00);
         if (Wire.endTransmission() != 0) return;
-        delayMicroseconds(I2C_TX_GRACE_TIME_MICROS);
+        delayMicroseconds(200);
 
         if (Wire.requestFrom(I2C_SLAVE_ADDRESS_WII_ATTACHMENT, WII_ATTACHMENT_ID_SIZE) != WII_ATTACHMENT_ID_SIZE)
             return;
@@ -124,62 +104,30 @@ void loop() {
         Wire.beginTransmission(I2C_SLAVE_ADDRESS_WII_ATTACHMENT);
         Wire.write(0x00); // fe 00 poll
         if (Wire.endTransmission() != 0) {
-            Serial.println("poll failed");
             return;
         }
         // FIXME Find how to detect when it's done rx/tx so I don't have to do this nonsense!
-        delayMicroseconds(I2C_TX_GRACE_TIME_MICROS);
+        delayMicroseconds(200);
 
         int size = Wire.requestFrom(I2C_SLAVE_ADDRESS_WII_ATTACHMENT, 6);
         if (size != 6) {
-            Serial.print("wrong size rx: ");
-            Serial.println(size, DEC);
             return;
         }
         NunchuckPacket packet[6];
         for (int i = 0; i < 6; i++) {
             if (Wire.available() <= 0) {
-                Serial.println("missing bytes in packet");
                 return;
             }
 
             packet->raw[i] = Wire.read();
         }
 
-        if (packet->accelerometerZAxisSignificantBits < 0x10) {
-            Serial.println("O---------------");
-        } else if (packet->accelerometerZAxisSignificantBits < 0x20) {
-            Serial.println("-O--------------");
-        } else if (packet->accelerometerZAxisSignificantBits < 0x30) {
-            Serial.println("--O-------------");
-        } else if (packet->accelerometerZAxisSignificantBits < 0x40) {
-            Serial.println("---O------------");
-        } else if (packet->accelerometerZAxisSignificantBits < 0x50) {
-            Serial.println("----O-----------");
-        } else if (packet->accelerometerZAxisSignificantBits < 0x60) {
-            Serial.println("-----O----------");
-        } else if (packet->accelerometerZAxisSignificantBits < 0x70) {
-            Serial.println("------O---------");
-        } else if (packet->accelerometerZAxisSignificantBits < 0x80) {
-            Serial.println("-------O--------");
-        } else if (packet->accelerometerZAxisSignificantBits < 0x90) {
-            Serial.println("--------O-------");
-        } else if (packet->accelerometerZAxisSignificantBits < 0xa0) {
-            Serial.println("---------O------");
-        } else if (packet->accelerometerZAxisSignificantBits < 0xb0) {
-            Serial.println("----------O-----");
-        } else if (packet->accelerometerZAxisSignificantBits < 0xc0) {
-            Serial.println("-----------O----");
-        } else if (packet->accelerometerZAxisSignificantBits < 0xd0) {
-            Serial.println("------------O---");
-        } else if (packet->accelerometerZAxisSignificantBits < 0xe0) {
-            Serial.println("-------------O--");
-        } else if (packet->accelerometerZAxisSignificantBits < 0xf0) {
-            Serial.println("--------------O-");
-        } else {
-            Serial.println("---------------O");
-        }
+        Joystick.button(6, !packet->notC);
+        Joystick.button(8, !packet->notZ);
+        Joystick.Z(packet->joystickXAxis);
+        Joystick.Zrotate(packet->joystickYAxis);
     }
 
-    delayMicroseconds(I2C_TX_GRACE_TIME_MICROS);
+    Serial.println(loopTime);
+    delay(200);
 }
