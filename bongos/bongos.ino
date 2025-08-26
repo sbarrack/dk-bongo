78a3a3ce2e96a2ea6b231ba49c488baa5463ec44
sbarrack <12619603+sbarrack@users.noreply.github.com>
2025-08-26 06:44:09 -0700

    comments & cleanup

--- bongos/bongos.ino
+++ bongos/bongos.ino
@@ -3,11 +3,11 @@
 // TODO Find c_cpp_properties.json in the git history and see if it's helpful.
 
 Nunchuck nunchuck1(Wire);
+// TODO Fix wiring for Wire1. (It's not my solder joint.)
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
-    Joystick.useManualSend(true);
 
     nunchuck1.setup();
 }
@@ -15,14 +15,6 @@ void setup() {
 void loop() {
     nunchuck1.loop();
 
-    digitalWrite(LED_BUILTIN, !nunchuck1.getZ());
-    Joystick.button(1, nunchuck1.getC());
-    Joystick.button(2, nunchuck1.getZ());
-    Joystick.X(nunchuck1.getX());
-    Joystick.Y(nunchuck1.getY());
-    Joystick.Z(nunchuck1.getAz());
-    Joystick.Zrotate(nunchuck1.getAx());
-    Joystick.sliderLeft(nunchuck1.getAy());
-
-    Joystick.send_now();
+    // TODO Input from Bongos.
+    // FIXME Output to GC.
 }
